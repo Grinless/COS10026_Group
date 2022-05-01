99ca1cbaf79c9d949abec198ef51ecea971e5ef6
Grinless <Cheshyrecat1337@gmail.com>
2022-05-01 13:47:56 +1000

    Added quizsuccess.php

---
+++ quizsuccess.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<meta name="description" content="Landing page of JSON informatic website">
+	<?php
+    	include("header.inc");
+    ?>
+    </head>
+<body>
+	<?php
+        include ("menu.inc");
+    ?>
+    <!--Content. START.-->
+    <br><br><br><br>
+    <section class="content">
+        <span>
+            <h> Quiz submission success:</h>
+            <p> Username/ID: &nbsp; Grade: &nbsp; Remaining attempts: <br> 
+            try again</p>
+        </span>
+    </section>
+    <br><br><br><br><br>
+    <!--Content. END.-->
+    <!--Footer. START.-->
+    <footer>
+       <?php
+	    include("footer.inc");
+	    ?>
+    </footer>
+    <!--Header END.-->
+</body>
+</html>
