b10c980dc7cf87dea73f9d6f9833be1bc0f431d4
Lofpt <102441789+Lofpt@users.noreply.github.com>
2022-05-05 14:21:05 +1000

    Update manage.php

--- manage.php
+++ manage.php
@@ -1,32 +1,16 @@
 <!DOCTYPE html>
 <htmsl lang="en">
 <head>
-	<meta charset="utf-8" />
 	<meta name="description" content="List all user data" />
-	<meta name="keyword" content="HTML, CSS, JavaScrippt" />
-	<meta name="author" content="Error 404 JSON not found" />
-	<title> Management </title>
-	<link rel="stylesheet" href="2.css"/>
-	<link rel="icon" type="image/x-icon" href="images/favicon.ico">
+	<?php
+	require("header.inc")
+	?>
 </head>
 <body class="Management">
 	<!--Header start.-->
-	<header>
-		<!-- Navigation bar and title-->
-		<h1> {JSON} </h1>
-		<figure>
-			<a href="https://json.org"> <img class="logo" src="images/JSON_logo.png" alt="JSON logo"> </a>
-			<figcaption class="logo">Click to go to json.org</figcaption>
-		</figure>
-		<nav>
-			<a href="index.html">Home</a>|
-			<a href="topic.html">More info</a>|
-			<a href="quiz.html">Quiz</a>|
-			<a href="enhancements.html">Enhancements</a>|
-			<a href="Management.php">Management Login</a>
-		</nav>
-	</header>
-	
+	<?php
+	include("menu.inc")
+	?>
 	<h1>Admin Login</h1>
 	
 	<form id="loginform" method="post" action="manage_queries.php">
@@ -50,5 +34,10 @@
 		echo"<p>Please enter correct username and password</p>"
 ?>
 	</form>
+	<footer>
+		<?php
+		include("footer.inc")
+		?>
+	</footer>
 </body>
 </html>
