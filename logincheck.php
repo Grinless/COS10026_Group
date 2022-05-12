da0ce5f227e6e592e06af51fa94034b77caf54b9
Lofpt <102441789+Lofpt@users.noreply.github.com>
2022-05-12 12:06:58 +1000

    Create logincheck.php
    place in every page that requires a login using requires() php

---
+++ logincheck.php
@@ -0,0 +1,5 @@
+<?php
+if (($_SESSION["username"] !== "JsonNotFound") and ($_SESSION["password"] !== "1234")){
+			  header("location: https://mercury.swin.edu.au/cos10026/s103993239/assign3/manage.php");
+		  }
+?>
