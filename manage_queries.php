7ef318a30a7985cc14bc3e4d6a2242ac2dd53499
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2022-05-18 22:53:26 +1000

    1

--- manage_queries.php
+++ manage_queries.php
@@ -28,11 +28,15 @@
 </form>
 
 <?php
+	if (isset($_POST['datatype'])) {
+		$datatype = ($_POST['datatype']);
+	}	
+
 	//connect to the dataabse
 	$host = "feenix-mariadb.swin.edu.au";
 	$user = "s103951761";
-	$pwd = "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo"
-	$sql_db = "s103951761_db";
+	$pwd = "ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo";
+	$s = "s103951761_db";
 	
 	require_once ("settings.php"); 	//connection info
 
@@ -49,32 +53,39 @@
 		// upon successful completion
 		$sql_table = "Attempts";
 		
-		print_$datatype 
-		
+
+	// run specific cmd
+		if ($datatype == "all") {
+			//echo "<p>$datatype</p>";
+			print_all();
 		}
-			
+		if ($datatype == "specificstudent") { 
+			print_specificstudent();
+		} 
+		if ($datatype == "fullmarks") { 
+			print_fullmarks();
+		} 
+
+		if ($datatype == "fail") { 
+			print_fail();
+		} 
+
+		if ($datatype == "delete") { 
+			print_delete();
+		} 
+
+		if ($datatype == "change") { 
+			print_change();
+		} 
+
+	
 		mysqli_close($conn);
 	}
-		
-	
-	if (isset($_POST['datatype'])) {
-		print_data($_POST['datatype']);
-	}
-
-	function print_data($datatype) {
-		if $datatype == "all"
-			//echo "<p>$datatype</p>";
-			
-		if $datatype == "specificstudent"
-		if $datatype == "fullmarks"
-		if $datatype == "fail"
-		if $datatype == "delete"
-		if $datatype == "change"
-	}
 	
 	
-	function print_all {
-		
+	
+	function print_all() {
+		echo "<p>Something is wrong with ", $query, "</p>";
 		//set up the SQL command to query or add data into the table
 		$query = "select Firstname, Lastname, StudentID, Date, Score, AttemptID FROM Attempts ORDER BY AttemptID";
 		
@@ -101,7 +112,68 @@
 			echo "</table>\n ";
 			
 			mysqli_free_result($results);
+		}
+
+
+///////////////////////////////////////
+///////////////////////////////////////
+//need to add a data entry. html how?//
+//the tbx and $data////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+
+	function print_specificstudent($data) {
+		$query = "select Firstname, Lastname, StudentID, Date, Score, AttemptID FROM attempts WHERE StudentID like '$data%'";
+	
+	
+		$results = mysqli_query($conn, $query);
+		
+		if(!$results) {
+			echo "<p class=\"wrong\">Unable to connect to the server</p>";
+		} else {
+			if (mysqli_fetch_assoc($results)["make"] == "") {
+				echo "<p class=\"blank\">There are no results</p>";
+			} else {
+				echo "<table border=\"1\">\n";
+				echo "<tr>\n "
+					."<th scope=\"col\">Make</th>\n "
+					."<th scope=\"col\">Model</th>\n "
+					."<th scope=\"col\">Price</th>\n "
+					."</th>\n ";
+				
+				
+				While ($row = mysqli_fetch_assoc($results)) {
+					echo "<tr>\n ";
+					echo "<td>",$row["make"],"</td>\n ";
+					echo "<td>",$row["model"],"</td>\n ";
+					echo "<td>",$row["price"],"</td>\n ";
+					echo "</tr>\n ";
+				}
+			}
+		}
+	
 	}
+
+	function print_fullmarks() {
+
+	}
+
+	function print_fail() {
+
+	}
+
+	function print_delete() {
+
+	}
+
+	function print_change() {
+
+	}
+
+
+
+
+
 ?>
 	
 
