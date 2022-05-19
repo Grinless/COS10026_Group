c0f253fee3a794807a92d7364674960f1a9e0924
Lofpt <102441789+Lofpt@users.noreply.github.com>
2022-05-19 15:02:40 +1000

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
 session_destroy();
-header("https://mercury.swin.edu.au/cos10026/s103993239/assign3/index.php");
+header("location: https://mercury.swin.edu.au/cos10026/s103993239/assign3/index.php");
 ?>
