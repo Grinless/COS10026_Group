a544180b9d7abedf3e8a8703699ce4174d5e3f26
MagmaArcade <102445046+MagmaArcade@users.noreply.github.com>
2022-05-26 13:03:57 +1000

    1

--- manage_display_query.php
+++ manage_display_query.php
@@ -1,9 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="description" content="List all user data" />
+	<?php require('header.inc');?>
+</head>
+<body class="Management">
+	<!--Header start.-->
+		<!-- Navigation bar and title-->
+		<?php include('menu.inc'); ?>
+	
+	<h2> User Quiz Data Results </h2>
+
+
 <?php
 	if (isset($_POST['datatype'])) {
 		$datatype = ($_POST['datatype']);
 	}
 	if (isset($_POST['studentid'])) {
-		$_STUDENT = ($_POST['studentid']);
+		$_STUDENTID = ($_POST['studentid']);
 	}
 	if (isset($_POST['attemptid'])) {
 		$_ATTEMPT = ($_POST['attemptid']);
@@ -11,17 +25,31 @@
 	if (isset($_POST['newscore'])) {
 		$_NEWSCORE = ($_POST['newscore']);
 	}
-
-
+	if (isset($_POST['studentname'])) {
+		$_STUDENTNAME = ($_POST['studentname']);
+	}
 		
+
+	if ($datatype == "blank") {
+		echo "<p>Error: Please select a field in the <a href=\"manage_form.php\">form</a></p>";
+	}
+
 	if ($datatype == "all") {
 		print_all();
 	}
 	if ($datatype == "specificstudent") { 
-		if ($_STUDENT == "") {
-			echo "<p>Error: Please fill in student information into the <a href=\"manage_form.php\">form</a></p>";
+		if ($_STUDENTNAME != "" and $_STUDENTNAME != "") {
+			echo "<p>Error: Please fill in student name or id information into the <a href=\"manage_form.php\">form</a></p>";
 		} else {
-			print_specificstudent($_STUDENT);
+			if ($_STUDENTID == "") {
+				if ($_STUDENTNAME == "") {
+					echo "<p>Error: Please fill in student name or id information into the <a href=\"manage_form.php\">form</a></p>";
+				} else {
+					print_specificstudentname($_STUDENTNAME);
+				}	
+			} else {
+				print_specificstudentid($_STUDENTID);
+			}
 		}
 	} 
 	if ($datatype == "fullmarks") { 
@@ -78,6 +106,8 @@ function print_all() {
 		if(!$results) {
 			echo "<p>Something is wrong with ", $query, "</p>";
 		} else {
+			echo "<p>All student scores</p>";
+
 			echo "<table border=\"1\">\n";
 			echo "<tr>\n "
 				."<th scope=\"col\">attempt_id</th>\n "
@@ -106,11 +136,17 @@ function print_all() {
 			mysqli_free_result($results);
 		}
 		mysqli_close($conn);
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
 	}
 	
 }
 
-Function print_specificstudent($_STUDENT) {
+
+Function print_specificstudentname($_STUDENTNAME) {
 	//connect to the dataabse	
 	require ("settings.php"); 	//connection info
 	
@@ -128,7 +164,7 @@ Function print_specificstudent($_STUDENT) {
 		$sql_table = "attempts";
 		
 		//set up the SQL command to query or add data into the table
-		$query = "SELECT attempt_id, attempt_number, first_name, last_Name, student_id, score, date_time FROM attempts WHERE student_id like '$_STUDENT%'";
+		$query = "SELECT attempt_id, attempt_number, first_name, last_Name, student_id, score, date_time FROM attempts WHERE first_name like '$_STUDENTNAME%'";
 
 		
 		$results = mysqli_query($conn, $query);
@@ -136,6 +172,8 @@ Function print_specificstudent($_STUDENT) {
 		if(!$results) {
 			echo "<p>Something is wrong with ", $query, "</p>";
 		} else {
+			echo "<p>Specific student scores</p>";
+
 			//if (mysqli_fetch_assoc($results)["attempt_id"] == "") {
 			//	echo "<p class=\"blank\">There are no results</p>";     			////Need to fix db no result
 			//} else {
@@ -171,6 +209,85 @@ Function print_specificstudent($_STUDENT) {
 		}	
 	}
 	mysqli_close($conn);
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
+
+
+}
+
+
+Function print_specificstudentid($_STUDENTID) {
+	//connect to the dataabse	
+	require ("settings.php"); 	//connection info
+	
+	$conn = @mysqli_connect($host, $user, $pwd, $sql_db);
+
+
+	//checks of connection is successful
+	if (!$conn) {
+		//displays an error message
+		echo "<p>Database connection failure</p>";
+	} else {
+
+
+		// upon successful completion
+		$sql_table = "attempts";
+		
+		//set up the SQL command to query or add data into the table
+		$query = "SELECT attempt_id, attempt_number, first_name, last_Name, student_id, score, date_time FROM attempts WHERE student_id like '$_STUDENTID%'";
+
+		
+		$results = mysqli_query($conn, $query);
+		
+		if(!$results) {
+			echo "<p>Something is wrong with ", $query, "</p>";
+		} else {
+			echo "<p>Specific student scores</p>";
+
+			//if (mysqli_fetch_assoc($results)["attempt_id"] == "") {
+			//	echo "<p class=\"blank\">There are no results</p>";     			////Need to fix db no result
+			//} else {
+
+				echo "<table border=\"1\">\n";
+					echo "<tr>\n "
+					."<th scope=\"col\">attempt_id</th>\n "
+					."<th scope=\"col\">attempt_number</th>\n "
+					."<th scope=\"col\">first_name</th>\n "
+					."<th scope=\"col\">last_Name</th>\n "
+					."<th scope=\"col\">student_id</th>\n "
+					."<th scope=\"col\">score</th>\n "
+					."<th scope=\"col\">date_time</th>\n "
+
+					."</th>\n ";
+				
+				
+				While ($row = mysqli_fetch_assoc($results)) {
+					echo "<tr>\n ";
+					echo "<td>",$row["attempt_id"],"</td>\n ";
+					echo "<td>",$row["attempt_number"],"</td>\n ";
+					echo "<td>",$row["first_name"],"</td>\n ";
+					echo "<td>",$row["last_Name"],"</td>\n ";
+					echo "<td>",$row["student_id"],"</td>\n ";
+					echo "<td>",$row["score"],"</td>\n ";
+					echo "<td>",$row["date_time"],"</td>\n ";
+					echo "</tr>\n ";
+				}
+				echo "</table>\n ";
+				
+				mysqli_free_result($results);
+			//}
+		}	
+	}
+	mysqli_close($conn);
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
+
 
 }
 
@@ -200,6 +317,8 @@ Function print_fullmarks() {
 		if(!$results) {
 			echo "<p>Something is wrong with ", $query, "</p>";
 		} else {
+			echo "<p>All students who scored 100% on their first attempt</p>";
+
 			//if (mysqli_fetch_assoc($results)["attempt_id"] == "") {
 			//	echo "<p class=\"blank\">There are no results</p>";     			////Need to fix db no result
 			//} else {
@@ -235,6 +354,12 @@ Function print_fullmarks() {
 		}	
 	}
 	mysqli_close($conn);
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
+
 
 }
 
@@ -250,13 +375,13 @@ Function print_fail() {
 		//displays an error message
 		echo "<p>Database connection failure</p>";
 	} else {
-
+		echo "<p>All students who scored under 50% on their second attempt</p>";
 
 		// upon successful completion
 		$sql_table = "attempts";
 		
 		//set up the SQL command to query or add data into the table
-		$query = "SELECT attempt_id, attempt_number, first_name, last_Name, student_id, score, date_time FROM attempts WHERE Attempt_id = 2 and score <= 50";
+		$query = "SELECT attempt_id, attempt_number, first_name, last_Name, student_id, score, date_time FROM attempts WHERE attempt_number = 2 and score <= 50";
 
 		
 		$results = mysqli_query($conn, $query);
@@ -299,7 +424,11 @@ Function print_fail() {
 		}	
 	}
 	mysqli_close($conn);
-
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
 }
 
 
@@ -315,7 +444,6 @@ function print_delete($_ATTEMPT) {
 		//displays an error message
 		echo "<p>Database connection failure</p>";
 	} else {
-		
 		//set up the SQL command to query or add data into the table
 		$query = "DELETE FROM attempts WHERE attempt_id = $_ATTEMPT";
 
@@ -331,6 +459,11 @@ function print_delete($_ATTEMPT) {
 													///Add a attempt deleted msg	
 	}
 	mysqli_close($conn);
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
 }
 
 
@@ -361,8 +494,12 @@ function print_change($_ATTEMPT, $_NEWSCORE) {
 		}	
 	}
 	mysqli_close($conn);
-
+	?>
+		<a href="manage_form.php">
+   		  <input type="submit" value="Return"/>
+   		</a>
+	<?php
 }
-
-
 ?>
+</body>
+</html>
--- manage_form.php
+++ manage_form.php
@@ -13,25 +13,28 @@
 	<br/>
 
 <form id="searchform" method="post" action="manage_display_query.php">
+	<fieldset>
 	<label for="datatype">View Scores</label>
 	<select name="datatype" id="datatype">
-		<option value="">--- Select Data Set ---</option>
+		<option value="blank">--- Select Data Set ---</option>
 		<option value="all">List all attempts</option>
-		<option value="specificstudent">List a specific student</option>  <!-- search student id -->
+		<option value="specificstudent">List a specific student (Student ID or Name)</option>  <!-- search student id -->
 		<option value="fullmarks">List all students who scored 100% on first attempt</option> <!-- id, first, last name-->
 		<option value="fail">List all attempts who scored under 50% on second attempt</option>
-		<option value="delete">Delete a student's attempts</option>
-		<option value="change">Change a student's score</option><!-- given st id and attempt number (so display attempts then select attempt) -->
+		<option value="delete">Delete a student's attempts (Attempt ID)</option>
+		<option value="change">Change a student's score (Attempt ID and New Score)</option><!-- given st id and attempt number (so display attempts then select attempt) -->
 	</select>
 
-	<p><label for="studentid">Enter Student ID</label>
+	<p><label for="studentid">Student ID: &nbsp &nbsp &nbsp &nbsp &nbsp</label>
 	   <input type="text" id="studentid" name="studentid" size="5"></p>
-	<p><label for="attemptid">Enter Attempt ID</label>	 			
-	   <input type="text" id="attemptid" name="attemptid" size="5"></p> 		<!-- needs to be fixed -->
-	<p><label for="newscore">Enter New Score</label>
+	<p><label for="studentname">Student Name: &nbsp &nbsp</label>
+	   <input type="text" id="studentname" name="studentname" size="5"></p>
+	<p><label for="attemptid">Attempt ID: &nbsp &nbsp &nbsp &nbsp &nbsp</label>	 			
+	   <input type="text" id="attemptid" name="attemptid" size="5"></p> 		
+	<p><label for="newscore">New Score: &nbsp &nbsp&nbsp &nbsp &nbsp</label>
 	   <input type="text" id="newscore" name="newscore" size="5"></p>
 
-
+	</fieldset>
 	<p><input type="submit" value="Search"/></p>
 </form>	
 </body>
